ead3dd378e7b6999b6bb3f8647a593dd34f3fbb3
NOUMSI ULRICH STEVE <38357057+ulrich-sun@users.noreply.github.com>
2026-02-12 15:25:51 -0500

    Create vagrant-stack-manager/install-git.bat

---
+++ vagrant-stack-manager/install-git.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Installing Git...
+winget install --id Git.Git -e --source winget
+if %errorLevel% neq 0 (
+    echo Git installation failed. Please install manually from https://git-scm.com/download/win
+    pause
+) else (
+    echo Git installed successfully! You may need to restart the script/terminal.
+    pause
+)
